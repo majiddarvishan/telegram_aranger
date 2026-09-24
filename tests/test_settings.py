39d2790559d610b0e0943ca7f8c4ea10410eeed6
Majid Darvishan <majiddarvishan@users.noreply.github.com>
2026-09-25 01:46:33 +0330

    test: cover default Telegram dialog limit

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -12,6 +12,7 @@ class SettingsDefaultsTests(unittest.TestCase):
         )
 
         self.assertEqual(settings.message_scroll_height, 620)
+        self.assertEqual(settings.telegram_dialog_limit, 100)
 
 
 if __name__ == "__main__":
