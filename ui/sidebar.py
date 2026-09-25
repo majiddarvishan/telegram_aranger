51e33a45d1ef71ce8af64de5cdd7522615258e7c
Majid Darvishan <majiddarvishan@users.noreply.github.com>
2026-09-25 22:44:09 +0330

    state: clear latest-message fallback on account switch

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -63,6 +63,7 @@ def _apply_account_selection(
     state["selected_chat_id"] = None
     state["messages"] = []
     state["message_fetch_error"] = None
+    state["message_auto_latest_chat_id"] = None
     state["dialogs"] = []
     state["force_refresh_dialogs"] = False
     state["telegram_user"] = None
